ceae8447d012211074070f2abb1adfeeeeb37bf2
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-09-17 01:20:21 +0200

    Added initial version of XlsEnergyRecord.

---
+++ energyRecordIntegrator/XlsEnergyRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace energyRecordIntegrator
+{
+    class XlsEnergyRecord
+    {
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private List<string> driversNames;
+        private List<string> driversComments;
+
+        public XlsEnergyRecord()
+        {
+
+        }
+    }
+}
